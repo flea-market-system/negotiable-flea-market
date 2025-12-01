7e8dcc87f1f0f199cda4b5f7791c9868941ac391
ko morikawa <morikouuu0523@gmail.com>
2025-12-01 16:31:35 +0900

    初回コミット

---
+++ src/main/java/com/example/negotiable_flea_market/NegotiableFleaMarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.negotiable_flea_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NegotiableFleaMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NegotiableFleaMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/negotiable_flea_market/NegotiableFleaMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.negotiable_flea_market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NegotiableFleaMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
