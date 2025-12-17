6ae235ea5c540ab84d7736a2f26be2be755dc898
ko morikawa <morikouuu0523@gmail.com>
2025-12-17 13:24:14 +0900

    adminUserService修正

--- src/main/java/com/example/negotiable_flea_market/repository/UserRepository.java
+++ src/main/java/com/example/negotiable_flea_market/repository/UserRepository.java
@@ -3,6 +3,8 @@ package com.example.negotiable_flea_market.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.negotiable_flea_market.entity.User;
@@ -13,5 +15,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
 	Optional<User> findByEmail(String email);
 
 	Optional<User> findByEmailIgnoreCase(String email);
+
+	@Query("SELECT AVG(r.rating) FROM Review r WHERE r.seller.id = :userId")
+	Double averageRatingForUser(@Param("userId") Long userId);
 
 }
